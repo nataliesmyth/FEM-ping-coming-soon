bf24c5850dea3a6fc28f250e4e4aa990ee9748d0
Nataliesmyth <nataliesmyth11@gmail.com>
2023-09-30 15:15:35 -0400

    added validation for invalid email addresses and styled error page

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ function validate(e) {
   e.preventDefault();
 
   const emailField = document.getElementById("mail");
+  const regex = /[^\s@]+@[^\s@]+\.[^\s@]+/;
   let valid = true;
 
-  if (!emailField.value) {
+  if (!emailField.value || !emailField.value.match(regex)) {
     const emailError = document.getElementById("emailError");
     emailError.classList.add("visible");
     emailField.classList.add("invalid");
